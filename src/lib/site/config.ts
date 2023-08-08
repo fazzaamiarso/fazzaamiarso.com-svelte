ac0d2ae4ae490a2378a8881c0272de37bfe4bb73
fazzaamiarso <fazzaamiarso@gmail.com>
2023-08-09 05:16:54 +0700

    initial setup

---
+++ src/lib/site/config.ts
@@ -0,0 +1,6 @@
+export const siteTitle = 'Fazza Razaq Amiarso | Frontend Developer';
+export const siteDescription =
+	'A frontend developer with focus on making accessible software with beautiful interfaces for everyone';
+export const siteUrl = 'fazzaamiarso.com';
+
+export const twitterHandle = '@faz_razq';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import * as config from '$lib/site/config';
+</script>
+
+<svelte:head>
+	<title>{config.siteTitle}</title>
+	<meta name="description" content={config.siteDescription} />
+	<!-- Open Graph START -->
+	<meta property="og:title" content={config.siteTitle} />
+	<meta property="og:url" content={config.siteUrl} />
+	<!-- Open Graph END  -->
+	<!-- Twitter card START -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content={config.twitterHandle} />
+	<!-- Twitter card END -->
+</svelte:head>
+
+<h1>Fazza Razaq Amiarso</h1>
