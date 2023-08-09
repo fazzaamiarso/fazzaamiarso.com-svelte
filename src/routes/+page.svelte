046fde4f7667433f8fa41167ad22f5456b06956b
fazzaamiarso <fazzaamiarso@gmail.com>
2023-08-10 00:24:26 +0700

    feat: implement drawer, but with some messy implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,4 +14,23 @@
 	<meta name="twitter:creator" content={config.twitterHandle} />
 	<!-- Twitter card END -->
 </svelte:head>
-<h1 class="text-heading-1 font-fira-sans text-brand-800">Frontend Developer Who Bring your Idea to the Web</h1>
+<div class="h-screen relative flex flex-col justify-center items-center">
+	<h1 class=" font-fira-sans text-brand-800 text-heading-3">
+		Frontend Developer Who Bring your Idea to the Web
+	</h1>
+	<div class="absolute bottom-0 left-0 w-full flex justify-between">
+		<div class="py-4 flex flex-col items-start text-body-lg">
+			<span class="font-bold text-gray-800">Fullstack</span><span class="text-gray-600"
+				>capable</span
+			>
+		</div>
+		<div class="py-4 flex flex-col items-start text-body-lg">
+			<span class="font-bold text-gray-800">Sharing</span><span class="text-gray-600"
+				>knowledge</span
+			>
+		</div>
+	</div>
+</div>
+<div>
+	<h2 class="text-heading-2">SOME RANDOM TEXT so it scrolls</h2>
+</div>
