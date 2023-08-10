8625d4ac68badd95c576d51bc5bd23fe0497a7ef
fazzaamiarso <fazzaamiarso@gmail.com>
2023-08-11 03:55:03 +0700

    chore: change icon to use lucide-svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,6 @@
 <script>
 	import * as config from '$lib/site/config';
+	import { MoveUpRight, SendHorizonal } from 'lucide-svelte';
 
 	const socialLinks = [
 		{ label: 'Linkedin', href: config.linkedinUrl },
@@ -14,8 +15,8 @@
 			Let’s talk or build awesome things together!
 		</h2>
 		<p class="sm:text-body-xl text-body-base text-gray-200 mb-12">
-			I’m currently looking for Frontend Developer Role. Don’t hesitate to reach me out to ask
-			questions, discussions, or just to say hello.
+			I’m currently looking for Frontend Developer Role. Don’t hesitate to reach me out to ask questions, discussions,
+			or just to say hello.
 		</p>
 		<a
 			href="mailto:fazzaamiarso@gmail.com"
@@ -23,41 +24,15 @@
 			>fazzaamiarso@gmail.com
 			<span
 				class="inline-flex items-center justify-center w-[35px] h-[35px] sm:w-[45px] sm:h-[45px] bg-brand-100 rounded-full"
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="w-6 h-6 text-gray-800 group-hover:-rotate-45 transition-transform"
-					aria-hidden="true"
-				>
-					<path
-						d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z"
-					/>
-				</svg>
+				><SendHorizonal aria-hidden="true" size="24" class="text-gray-800" />
 			</span>
 		</a>
 		<ul class="flex flex-wrap gap-16 items-center pb-40">
 			{#each socialLinks as link}
 				<li class="text-body-lg sm:text-body-xl text-gray-100">
-					<a
-						href={link.href}
-						referrerpolicy="no-referrer"
-						target="_blank"
-						class="flex items-center gap-1"
+					<a href={link.href} referrerpolicy="no-referrer" target="_blank" class="flex items-center gap-1"
 						>{link.label}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="w-6 h-6"
-							aria-hidden="true"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M8.25 3.75H19.5a.75.75 0 01.75.75v11.25a.75.75 0 01-1.5 0V6.31L5.03 20.03a.75.75 0 01-1.06-1.06L17.69 5.25H8.25a.75.75 0 010-1.5z"
-								clip-rule="evenodd"
-							/>
-						</svg>
+						<MoveUpRight color="currentColor" />
 					</a>
 				</li>
 			{/each}
@@ -66,9 +41,7 @@
 			<div class="text-body-sm sm:text-body-base font-bold text-gray-200">
 				© 2023 Fazza Razaq Amiarso --- Bogor, Indonesia
 			</div>
-			<a href="/#?credit_open=true" class="text-body-sm sm:text-body-base font-bold text-gray-200"
-				>Credits</a
-			>
+			<a href="/#?credit_open=true" class="text-body-sm sm:text-body-base font-bold text-gray-200">Credits</a>
 		</div>
 	</div>
 </footer>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { ExternalLink } from 'lucide-svelte';
 	import myPic from '$lib/assets/fazza-about.png';
+
+	const otherLinks = [
+		{ type: 'Blog', category: 'Web development', link: 'blog.fazzaamiarso.com' },
+		{ type: 'Blog', category: 'Journal', link: 'journal.fazzaamiarso.com' }
+	];
 </script>
 
 <svelte:head>
@@ -13,8 +19,8 @@
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 			</p>
 			<p class="text-body-base lg:text-body-lg text-gray-900">
-				Donec vulputate lorem urna, porttitor tincidunt dolor tincidunt et. Ut vel urna volutpat,
-				tincidunt felis ac, tristique enim. Nullam at mi a ligula faucibus dignissim eu eu sapien.
+				Donec vulputate lorem urna, porttitor tincidunt dolor tincidunt et. Ut vel urna volutpat, tincidunt felis ac,
+				tristique enim. Nullam at mi a ligula faucibus dignissim eu eu sapien.
 			</p>
 		</div>
 		<div class="lg:basis-[60%]">
@@ -22,58 +28,22 @@
 		</div>
 	</div>
 	<div>
-		<h2 class="text-heading-4 font-bold text-brand-800">Checkout My Other Endeavors</h2>
-		<p class="text-gray-700 text-body-base mb-7">
-			I love sharing my knowledge with others and writing in general. So here are some of my
-			endeavors
+		<h2 class="text-heading-5 sm:text-heading-4 font-bold text-brand-800 mb-1">Checkout My Other Endeavors</h2>
+		<p class="text-gray-700 text-body-base mb-7 sm:text-body-lg">
+			I love sharing my knowledge with others and writing in general. So here are some of my endeavors
 		</p>
 		<ul class="space-y-12">
-			<li class="grid grid-cols-3 w-full">
-				<div class="col-span-1">
-					<div class="text-heading-5 font-bold text-brand-500">Blog</div>
-					<div class="text-body-base text-gray-600">Web development</div>
-				</div>
-				<a
-					href="#link-1"
-					class="inline-flex items-center gap-1 text-body-base font-bold text-gray-700 col-span-2"
-					>blog.fazzaamiarso.com <svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						class="w-5 h-5"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</a>
-			</li>
-			<li class="grid grid-cols-3">
-				<div class="col-span-1">
-					<div class="text-heading-5 font-bold text-brand-500">Blog</div>
-					<div class="text-body-base text-gray-600">Journal</div>
-				</div>
-				<a
-					href="#link-2"
-					class="inline-flex items-center gap-1 text-body-base font-bold text-gray-700 col-span-2"
-					>journal.fazzaamiarso.com <svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						class="w-5 h-5"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</a>
-			</li>
+			{#each otherLinks as item}
+				<li class="grid grid-cols-3 w-full">
+					<div class="col-span-1">
+						<div class="text-heading-5 font-bold text-brand-500">{item.type}</div>
+						<div class="text-body-base text-gray-600">{item.category}</div>
+					</div>
+					<a href="#link-1" class="inline-flex items-center gap-1 text-body-base font-bold text-gray-700 col-span-2"
+						>{item.link} <ExternalLink aria-hidden="true" size="20"/>
+					</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </div>
