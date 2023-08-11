a1afd4e7385303111c21966af624594032299cf4
fazzaamiarso <fazzaamiarso@gmail.com>
2023-08-12 01:57:45 +0700

    chore: refactor

---
+++ src/lib/components/skills-list.svelte
@@ -0,0 +1,43 @@
+<script>
+	import {
+		typescript,
+		svelte,
+		javascript,
+		next,
+		css,
+		html,
+		react,
+		tailwind,
+		astro,
+		figma,
+		gsap as gsapIcon,
+		redux,
+		jest,
+		playwright,
+		cypress
+	} from '$lib/assets/icons';
+	const skills = [
+		{ name: 'Svelte/Sveltekit', icon: svelte },
+		{ name: 'Typescript', icon: typescript },
+		{ name: 'Javascript', icon: javascript },
+		{ name: 'Next.js', icon: next },
+		{ name: 'CSS', icon: css },
+		{ name: 'HTML', icon: html },
+		{ name: 'React', icon: react },
+		{ name: 'Tailwind', icon: tailwind },
+		{ name: 'Astro', icon: astro },
+		{ name: 'Figma', icon: figma },
+		{ name: 'GSAP', icon: gsapIcon },
+		{ name: 'Redux', icon: redux },
+		{ name: 'Jest', icon: jest },
+		{ name: 'Playwright', icon: playwright },
+		{ name: 'Cypress', icon: cypress }
+	];
+</script>
+
+<ul class="grid grid-cols-2 sm:grid-cols-3 items-center gap-8 flex-wrap">
+	{#each skills as skill}
+		<li data-animate="skill" class="flex items-center text-body-base text-gray-700 font-bold gap-2 break-all">
+			<img src={skill.icon} alt="" class="w-8 md:w-10 lg:w-12" />{skill.name}
+		</li>{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,20 @@
 <script lang="ts">
 	import * as config from '$lib/site/config';
 	import { ArrowDownCircle } from 'lucide-svelte';
-	import {
-		typescript,
-		svelte,
-		javascript,
-		next,
-		css,
-		html,
-		react,
-		tailwind,
-		astro,
-		figma,
-		gsap as gsapIcon,
-		redux,
-		jest,
-		playwright,
-		cypress
-	} from '$lib/assets/icons';
+
 	import { lenis } from '$lib/lenis';
 	import { onMount } from 'svelte';
 	import gsap from '$lib/gsap';
-
-	const skills = [
-		{ name: 'Svelte/Sveltekit', icon: svelte },
-		{ name: 'Typescript', icon: typescript },
-		{ name: 'Javascript', icon: javascript },
-		{ name: 'Next.js', icon: next },
-		{ name: 'CSS', icon: css },
-		{ name: 'HTML', icon: html },
-		{ name: 'React', icon: react },
-		{ name: 'Tailwind', icon: tailwind },
-		{ name: 'Astro', icon: astro },
-		{ name: 'Figma', icon: figma },
-		{ name: 'GSAP', icon: gsapIcon },
-		{ name: 'Redux', icon: redux },
-		{ name: 'Jest', icon: jest },
-		{ name: 'Playwright', icon: playwright },
-		{ name: 'Cypress', icon: cypress }
-	];
+	import SkillsList from '$lib/components/skills-list.svelte';
 
 	onMount(() => {
 		gsap
 			.timeline({
 				scrollTrigger: {
-					trigger: "[data-animate='skill']",
+					trigger: "[data-animate='skill']"
 				}
 			})
-			.from("[data-animate='skill']", { stagger: 0.15, y: 50, opacity: 0});
+			.from("[data-animate='skill']", { stagger: 0.15, y: 50, opacity: 0 });
 	});
 </script>
 
@@ -63,15 +30,15 @@
 	<meta name="twitter:creator" content={config.twitterHandle} />
 	<!-- Twitter card END -->
 </svelte:head>
-<div class="h-screen relative flex flex-col justify-center items-center">
+<div class="mb-32 h-screen relative flex flex-col justify-center items-center">
 	<h1 class="font-fira-sans w-10/12 mx-auto text-brand-800 text-heading-3 sm:text-heading-2 lg:text-heading-1">
 		Frontend Developer Who Bring your Idea to the Web
 	</h1>
-	<div class="absolute bottom-0 left-0 w-full flex items-end justify-between">
-		<div class="py-4 flex flex-col items-start text-body-lg">
+	<div class="py-4 absolute bottom-0 left-0 w-full flex items-end justify-between">
+		<div class="flex flex-col items-start text-body-lg">
 			<span class="font-bold text-gray-800">Fullstack</span><span class="text-gray-600">capable</span>
 		</div>
-		<div class="py-4">
+		<div class="">
 			<a
 				href="#projects"
 				on:click={(e) => {
@@ -82,21 +49,15 @@
 				<ArrowDownCircle size="40" aria-hidden="true" class="animate-bounce text-gray-800" />
 			</a>
 		</div>
-		<div class="py-4 flex flex-col items-start text-body-lg">
+		<div class=" flex flex-col items-start text-body-lg">
 			<span class="font-bold text-gray-800">Sharing</span><span class="text-gray-600">knowledge</span>
 		</div>
 	</div>
 </div>
-<div class="pt-32">
+<div class="pb-32">
 	<h2 id="projects" class="text-heading-4 lg:text-heading-2 font-bold text-brand-800">Selected Projects.</h2>
 </div>
-<div class="pt-14 pb-20">
+<div class="pb-32">
 	<h2 class="text-heading-4 lg:text-heading-2 font-bold text-brand-800 mb-6">Technologies.</h2>
-
-	<ul class="grid grid-cols-2 sm:grid-cols-3 items-center gap-8 flex-wrap">
-		{#each skills as skill}
-			<li data-animate="skill" class="flex items-center text-body-base text-gray-700 font-bold gap-2 break-all">
-				<img src={skill.icon} alt="" class="w-8 md:w-10 lg:w-12" />{skill.name}
-			</li>{/each}
-	</ul>
+	<SkillsList />
 </div>
