a7cffa7790c41ffe117449435fb013a8b2361207
fazzaamiarso <fazzaamiarso@gmail.com>
2023-09-04 07:12:15 +0700

    feat: implement horizontal timeline

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import mePic from '$lib/assets/fazza-about.png';
 	import gsap, { horizontalLoop } from '$lib/gsap';
 	import { onMount } from 'svelte';
 
@@ -24,11 +25,31 @@
 				toggleActions: 'play none reverse none'
 			}
 		});
+
+		const horizontalPanels = gsap.utils.toArray<HTMLElement>('.h-panel');
+
+		const timelineLength = document.querySelector<HTMLElement>('.h-timeline .h-container')?.scrollWidth ?? 0;
+
+		gsap.to(horizontalPanels, {
+			ease: 'none',
+			x: () =>
+				-1 *
+				(timelineLength -
+					(document.querySelector<HTMLElement>('.h-timeline .h-container')?.getBoundingClientRect()?.left ?? 0)),
+			scrollTrigger: {
+				pin: true,
+				trigger: '.h-timeline',
+				scrub: true,
+				markers: true,
+				end: () => '+=' + (timelineLength - window.innerWidth),
+				onUpdate: (self) => console.log(self.progress)
+			}
+		});
 	});
 </script>
 
-<div class="flex flex-col pt-32 h-screen overflow-hidden">
-	<section class="leading-normal text-gray-800">
+<section class="flex flex-col pt-32 h-screen overflow-hidden">
+	<div class="leading-normal text-gray-800">
 		<div class="marquee flex flex-nowrap gap-2 whitespace-nowrap">
 			<p class="text-[10vw]">Hello!</p>
 			<p class="text-[10vw]">안녕!</p>
@@ -43,9 +64,59 @@
 			<p class="text-[10vw]">こんにちは!</p>
 			<p class="text-[10vw]">你好!</p>
 		</div>
-	</section>
+	</div>
 	<div class="scroll-more layout mt-auto pb-8 border-l-4 pl-4 border-brand-700 text-gray-800">
 		<p class="text-body-lg">Scroll to</p>
 		<p class="text-body-lg">discover more</p>
 	</div>
-</div>
+</section>
+<section class="flex items-center justify-between pt-32 layout">
+	<div class="basis-1/2">
+		<h2 class="text-heading-1">I'm Fazza</h2>
+		<p class="text-heading-3 font-dm-sans">A Fullstack-able Frontend developer from Indonesia.</p>
+	</div>
+	<img src={mePic} alt="" />
+</section>
+<section class="flex items-center justify-between pt-32 layout">
+	<h2 class="text-heading-2">Why?</h2>
+	<p class="text-body-xl basis-1/2">
+		I love building stuff on the web because I can freely express my Idea. It’s like having your own tiny space that
+		show you exist in this world. Sharing knowledge and opinion, build useful application, craft stunning visual
+		experience, make new friends and connections.
+	</p>
+</section>
+<section class="h-timeline pt-40 layout">
+	<h2 class="text-heading-2">How it all started?</h2>
+	<div class="h-container flex flex-nowrap ml-auto w-max py-32 gap-32">
+		<div class="h-panel ring-brand-700">
+			<p class="text-body-xl">
+				I was introduced to programming with Scratch in my Elementary School Robotic’s lesson. As a curious kid, it
+				piqued my interest and made me join the school’s robotic club. Eventually, I was selected to compete on a few
+				robotic competition.
+			</p>
+		</div>
+		<div class="h-panel ring-brand-700">
+			<p class="text-body-xl">
+				Continuing the journey, I delved a bit into C++ with Arduino because I was responsible for programming a Sumo
+				Robot in High School Robotic Club. I compete on a decent amount of Robotic competition across my time in
+				Highschool, I even go to Singapore twice to represent Indonesia.
+			</p>
+		</div>
+		<div class="h-panel ring-brand-700">
+			<p class="text-body-xl">
+				I quickly lost interest in C++ at that time because it was so difficult and I don’t have the passion yet.
+				Somehow, I got interested in Python after a quick workshop from a guest on my school
+			</p>
+			<p class="text-body-xl">
+				And I dropped it too. Later on, I was amazed by Web development in the end of High School from seeing a Youtube
+				Video and unsurprisingly keep going until present.
+			</p>
+		</div>
+	</div>
+</section>
+<section class="pt-32 layout">
+	<p class="text-body-xl">
+		I strive to help and guide others on their Web Development journey. Why? It’s because I know how challenging it’s to
+		learn web development, especially if they don’t have mentor or guidance.
+	</p>
+</section>
